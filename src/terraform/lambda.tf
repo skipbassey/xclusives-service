a2c72eddff41dd87011cb159760a8069f8b0ad3b
okwongbassey <skip.bassey@duke-energy.com>
2020-12-21 22:20:43 -0500

    removed iam xclusives role

--- src/terraform/lambda.tf
+++ src/terraform/lambda.tf
@@ -10,7 +10,7 @@ locals {
 
 resource "aws_lambda_function" "get_lambda" {
   function_name = local.lambda_function_name
-  role          = aws_iam_role.iam_for_xclusives_lambda.arn
+  role          = local.iam_for_xclusives_lambda
   handler       = local.lambda_handler
   s3_bucket     = aws_s3_bucket.lambda_bucket.id
   s3_key        = aws_s3_bucket_object.lambda_upload_zip.key
--- src/terraform/locals.tf
+++ src/terraform/locals.tf
@@ -3,4 +3,5 @@ locals {
   environment  = "dev"
   product_tags = map("xclusives", "app")
   region       = "us-east-1"
+  iam_for_xclusives_lambda = "arn:aws:iam::695738586291:role/iam_for_exclusives_lambda"
 }
